41946e1634091521460f2196c7ee418953e3d29c
Андрей Катаев <crovolk@yandex.ru>
2020-11-17 16:55:25 +0300

    Home work

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,6 +6,21 @@ import javafx.scene.control.TextField;
 
 public class Controller {
 
+    @FXML
+    private ListView<String> messageList;
+    @FXML
+    private TextField textField;
+    @FXML
+    private ListView<String> listContact;
+
+    @FXML
+    public void sendMessage() {
+        String text = textField.getText();
+        if (!text.isBlank()){
+            messageList.getItems().add(text);
+        }
+        textField.setText("");
+    }
 
 
 }
